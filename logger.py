27cfe51fde9f7cba22ce2e05900179a7d623f1fe
MulikaDEV <igor.data.analytics@gmail.com>
2025-11-06 15:13:00 +0100

    Remove local config and init files from Git tracking

--- logger.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-def setup_logger(level="INFO"):
-    logging.basicConfig(
-        level=getattr(logging, level),
-        format="%(asctime)s [%(levelname)s] %(message)s"
-    )
-    return logging.getLogger()
